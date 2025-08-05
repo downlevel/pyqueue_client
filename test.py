f01c6fbf22504020ce74bec7f392c7c790fdcc96
downlevel <maurogiulivi@gmail.com>
2025-08-05 09:15:35 +0200

    First commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from pyqueue_client import PyQueueClient
+
+notifier = PyQueueClient(queue_file="queue.json")
+
+notifier.add_message(message={
+        "message_field_1": "Message Field Value 1",
+        "message_field_2": "Message Field Value 2",
+})
+
+messages = notifier.get_messages()
+print(messages)
