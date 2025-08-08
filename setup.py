dcd79a2565170ae4e128bad7e05b8ac73c251b78
downlevel <maurogiulivi@gmail.com>
2025-08-08 23:55:29 +0200

    Updated version lib

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyqueue-client",
-    version="1.0.1",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.0",
